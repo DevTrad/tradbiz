9acd198b21494c5374b6300cca44d341593f4d28
flyingfisch <flyingfisch@toppagedesign.com>
2014-09-15 17:20:14 -0400

    fixed footer issue

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('feature')
-<h1>{{ $user->first_name}} {{ $user->last_name }}'s Profile</h1>
+<h1>{{{ $user->first_name }}} {{{ $user->last_name }}}'s Profile</h1>
 @stop
 
 @section('content')
