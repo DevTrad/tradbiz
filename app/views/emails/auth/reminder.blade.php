19be9ca8d7d1413bac59c62974a96b09901d0905
flyingfisch <flyingfisch@toppagedesign.com>
2014-09-19 14:23:30 -0400

    changed pw reminder email

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,8 +7,11 @@
 		<h2>Password Reset</h2>
 
 		<div>
-			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.<br/>
+			<h2>{{ link_to('password/reset/' . $token, 'Reset Your Password') }}</h2>
 			This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.
+		</div>
+		<div>
+			NOTE: If you did not attempt to reset your password, simply ignore this email.
 		</div>
 	</body>
 </html>
